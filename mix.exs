4eed24d5a80cc3170b75fcf7bda7d47d92372834
Takayuki Matsubara <takayuki.1229@gmail.com>
2022-02-25 21:17:39 +0900

    bump version to 1.0.5
    - it removes some deprecation warnings

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Breadcrumble.Mixfile do
   def project do
     [
       app: :breadcrumble,
-      version: "1.0.4",
+      version: "1.0.5",
       elixir: "~> 1.9",
       description: "Elixir port of Breadcrumble library",
       package: [
